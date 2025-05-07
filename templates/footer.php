386b73e3db9f20ed8c4f4c3784a88018e4ba9b95
hsary <zxcone@inbox.ru>
2025-05-07 20:35:38 +0500

    Create footer.php

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+</div>
+    <footer class="mt-5 py-3">
+        <div class="container text-center">
+            &copy; <?= date('Y') ?> <?= SITE_NAME ?>
+        </div>
+    </footer>
+    <script src="/assets/js/script.js"></script>
+</body>
+</html>
