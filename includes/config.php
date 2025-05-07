47a4599c884d5a02baaeb325e4cf95559e399ee6
hsary <zxcone@inbox.ru>
2025-05-07 20:40:26 +0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,11 @@
 <?php
-// Настройки базы данных
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'gruzovozoff');
-
-// Настройки администратора
 define('ADMIN_LOGIN', 'admin');
 define('ADMIN_PASS', 'gruzovik2024');
 
-// Другие настройки
 define('SITE_NAME', 'Грузовозофф');
 session_start();
 ?>
